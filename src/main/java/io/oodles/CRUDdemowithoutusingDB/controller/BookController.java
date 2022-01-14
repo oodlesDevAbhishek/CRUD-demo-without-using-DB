3ebd6366e0af6e5fe881e3cfbb7834e21acc66a7
Abhishek Gupta <abhishek@abhishek.Gupta>
2022-01-14 12:54:57 +0530

    return ResponseEntity Object with approppriate HttpStatus Code and handle Exeception

--- src/main/java/io/oodles/CRUDdemowithoutusingDB/controller/BookController.java
+++ src/main/java/io/oodles/CRUDdemowithoutusingDB/controller/BookController.java
@@ -1,8 +1,11 @@
 package io.oodles.CRUDdemowithoutusingDB.controller;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,32 +25,72 @@ public class BookController {
 	
 	//get all book handler  
 	@GetMapping("/books")
-	public List<Book> getBooks(){
+	public ResponseEntity<?>getBooks(){
 		
-		return bookService.getAllBooks();
+		List<Book> ls= bookService.getAllBooks();
+		if(ls.size() > 0) {
+			return new ResponseEntity<List<Book>>(ls ,HttpStatus.OK );
+			
+		}
+		else
+			return new ResponseEntity<>("Record not Found In dataBase" , HttpStatus.NOT_FOUND);
 	}
+	
+	
 	//get single book handler
 	@GetMapping("/book/{id}")
-	public Book findByName(@PathVariable("id") int id) {
-		return bookService.getBookById(id);
+	public ResponseEntity<?> findByName(@PathVariable("id") int id) {
+		Book book=null;
+		try{
+			book= bookService.getBookById(id);
+			return new ResponseEntity<Book>(book , HttpStatus.OK);
+			
+		}
+		catch(NoSuchElementException ex) {
+			return new ResponseEntity<>("sorry record not find  by id . pls try Again later ..!", HttpStatus.NOT_FOUND);
+		}
 	}
+	
+	
 	//insert new book handler
 	@PostMapping("/book/add")
-	public Book addBook(@RequestBody Book book) {
-		Book b=bookService.addBook(book);
-		return b;
+	public ResponseEntity<?> addBook(@RequestBody Book book){
+		try {
+		return new ResponseEntity<>("Record added into the Database",HttpStatus.OK);
+		}
+		catch(Exception ex) {
+			ex.printStackTrace();
+			return new ResponseEntity<>("Record Not inserted ! pls try agian later...",HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
+	
+	
+	//Update book handler
+		@PutMapping("/book/{bookId}")
+		public ResponseEntity<Book> updateBook(@RequestBody Book book ,@PathVariable("bookId")int id ) {
+			Book b;
+			try {
+			     b = bookService.updateBook(book, id);
+			     return  new ResponseEntity<Book>(b,HttpStatus.OK);
+			}
+			catch(Exception ex) {
+				ex.printStackTrace();
+				return new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
+			}
+	}	
+		
+		
 	//delete book handler
 	@DeleteMapping("/book/{bookId}")
-	public Book deleteBook(@PathVariable("bookId") int bookId) {
-		Book b =bookService.deleteBook(bookId);
-		return b;
-	}
-	//Update book handler
-	@PutMapping("/book/{bookId}")
-	public Book updateBook(@RequestBody Book book ,@PathVariable("bookId")int id ) {
-		return bookService.updateBook(book, id);
-	}
+	public ResponseEntity<Boolean> deleteBook(@PathVariable("bookId") int bookId) {
+		try {
+			return new ResponseEntity<Boolean>(true,HttpStatus.OK);
+		}
+		catch(Exception ex) {
+			ex.printStackTrace();
+			return new ResponseEntity<Boolean>(false ,HttpStatus.NOT_FOUND);
+		}
+	}		
 	
 	
 }
